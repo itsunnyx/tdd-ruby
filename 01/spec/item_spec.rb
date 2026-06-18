3c8601f241cf2e119eae7cb6eec21d25969b461b
itsunnyx <nanthawat.sitt@gmail.com>
2026-06-18 16:46:53 +0700

    edit cart test

--- 01/spec/item_spec.rb
+++ 01/spec/item_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/item'
 RSpec.describe Cart do
   it 'Given have minus price, should be error message "price must be positive"' do
     expect {
-      Item.new(name: "Apple", price: -200)
+      Item.new(name: "Apple", price: -100)
       }.to raise_error(RuntimeError, 'price must be positive')
   end
 end
